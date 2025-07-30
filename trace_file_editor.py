dea922038f0394ad2a988a2210b282209806af1c
NicholasLowe070627 <nicholaslowe070627@gmail.com>
2025-07-30 13:37:52 +1200

    added a function to remove thee feed rate in specific lines

--- trace_file_editor.py
+++ trace_file_editor.py
@@ -6,8 +6,12 @@ version 1
 """
 
 
+
+
 from tkinter import *
 from tkinter import filedialog
+
+
 
 
 class trace_editor():
@@ -25,86 +29,88 @@ class trace_editor():
         self.container.rowconfigure(2, weight=17)
         self.container.columnconfigure(1, weight=3)
         self.container.columnconfigure(3, weight =2 )
-        
-        
-        
+       
+       
+       
         self.title = Label(self.container, text = "Trace file editor", font = self.style)
         self.title.grid(row = 0, column= 1, columnspan = 4)
 
+
         self.spacer = Frame(self.container, bg = "black")
         self.spacer.grid(row =1 ,column=0, columnspan= 4, sticky= "news")
-        
+       
         self.number_bar = Canvas(self.container, width = 50)
         self.number_bar.grid(row = 2, column= 0, sticky="news")
-        
+       
         self.code_bar = Canvas(self.container, width = 300, bg = "white")
         self.code_bar.grid(row = 2, column = 1, sticky="nsew")
        
         self.scroll = Scrollbar(self.container, orient=VERTICAL, command=self.scroll_both)
         self.scroll.grid(row=2, column=2, sticky="ns")
-        
+       
         self.code_bar.configure(yscrollcommand=self.scroll.set)
         self.number_bar.configure(yscrollcommand=self.scroll.set)
         self.code_bar.bind_all("<MouseWheel>", self.mouse_scroll)
         self.number_bar.bind_all("<MouseWheel>", self.mouse_scroll)
 
+
         self.button_container = Frame(self.container)
         self.button_container.grid(row=2, column= 3, sticky="news")
         self.button_container.columnconfigure(0, weight= 1, pad = 10)
         self.button_container.columnconfigure(1, weight= 1, pad = 10)
-        
+       
         self.open_button = Button(self.button_container, text="Open File", command=self.open_file)
         self.open_button.grid(row = 0, column = 0, columnspan= 2)
-        
+       
         self.redundant = Button(self.button_container, text = "remove redundant code", command=self.remove)
         self.redundant.grid(row = 1, column= 1, sticky="news", padx=10, pady=5)
-        
+       
         self.retract_inp = Entry(self.button_container)
         self.retract_inp.grid(row= 2, column= 0, sticky="news", padx=10, pady=5)
-        self.retract_button = Button(self.button_container, text = "set retraction height")
+        self.retract_button = Button(self.button_container, text = "set retraction height", command = lambda: self.change(f"G00 Z{self.retract_inp.get()}"))
         self.retract_button.grid(row = 2, column= 1, sticky="news", padx=10, pady=5)
        
         self.trace_feed_inp = Entry(self.button_container)
         self.trace_feed_inp.grid(row= 3, column= 0, sticky="news", padx=10, pady=5)
-        self.trace_feed_button = Button(self.button_container, text = "set trace feed rate")
+        self.trace_feed_button = Button(self.button_container, text = "set trace feed rate", command = lambda: self.change(f"G01 X"))
         self.trace_feed_button.grid(row = 3, column= 1, sticky="news", padx=10, pady=5)
-        
+       
         self.RPM_inp = Entry(self.button_container)
         self.RPM_inp.grid(row= 4, column= 0, sticky="news", padx=10, pady=5)
         self.RPM_button = Button(self.button_container, text = "set Drill RPM")
         self.RPM_button.grid(row = 4, column= 1, sticky="news", padx=10, pady=5)
-        
-        self.retraction_speed_inp = Entry(self.button_container)
-        self.retraction_speed_inp.grid(row= 5, column= 0, sticky="news", padx=10, pady=5)
-        self.retraction_speed_button = Button(self.button_container, text = "set retraction speed")
-        self.retraction_speed_button.grid(row = 5, column= 1, sticky="news", padx=10, pady=5)
-        
+       
+        self.drill_speed_inp = Entry(self.button_container)
+        self.drill_speed_inp.grid(row= 5, column= 0, sticky="news", padx=10, pady=5)
+        self.drill_speed_button = Button(self.button_container, text = "set drill speed", command= lambda: self.has_feed(f"G01 Z-{self.drill_speed_inp.get()}"))
+        self.drill_speed_button.grid(row = 5, column= 1, sticky="news", padx=10, pady=5)
+       
         self.trace_depth_inp = Entry(self.button_container)
         self.trace_depth_inp.grid(row= 6, column= 0, sticky="news", padx=10, pady=5)
         self.trace_depth_button = Button(self.button_container, text = "set trace depth")
         self.trace_depth_button.grid(row = 6, column= 1, sticky="news", padx=10, pady=5)
-        
+       
         self.new_name = Label(self.button_container, text = "New file name", font = self.style)
         self.new_name.grid(row = 7, column= 0, sticky="news", padx=10, pady=5)
         self.new_name_inp = Entry(self.button_container)
         self.new_name_inp.grid(row= 8, column= 0, sticky="news", padx=10, pady=5)
         self.new_name_button = Button(self.button_container, text = "Export as new file")
         self.new_name_button.grid(row = 8, column= 1, sticky="news", padx=10, pady=5)
-        
-        
+       
+       
     def mouse_scroll(self,event):
         if event.delta:
             self.code_bar.yview_scroll(int(-1 * (event.delta / 120)), "units")
             self.number_bar.yview_scroll(int(-1 * (event.delta / 120)), "units")
-        
+       
     def scroll_both(self, *args):
         self.code_bar.yview(*args)
         self.number_bar.yview(*args)
-        
+       
     def open_file(self):
         self.code_bar.delete("all")
         self.number_bar.delete("all")
-                
+               
         self.file_path = filedialog.askopenfilename(
             title = "Select a file  ",
             filetypes = [("Text files", "*.txt"), ("All files", "*.*")]
@@ -114,51 +120,88 @@ class trace_editor():
                 self.file_content = file.read()
         self.lines = self.file_content.splitlines()
         self.display()
-        
+       
     def display(self):    
         self.code_bar.delete("all")
         self.number_bar.delete("all")
         line_height = 20
         y = 0
 
+
         for i, line in enumerate(self.lines):
             # Draw code line
             self.code_bar.create_text(5, y, anchor="nw", text=line, font="Arial 12")
 
+
             # Draw corresponding line number
             self.number_bar.create_text(5, y, anchor="nw", text=str(i + 1), font="Arial 12")
 
+
             y += line_height
+
 
         # Draw vertical separator line in number_bar
         self.number_bar.create_line(49, 0, 49, y, fill="black")
 
+
         # Update scroll region so scrollbars work correctly
         self.code_bar.configure(scrollregion=(0, 0, 400, y))
         self.number_bar.configure(scrollregion=(0, 0, 50, y))
-    
+   
     def remove(self):
         self.remove_index = []
         self.defult = ["G21", "G90", "G00 Z2.5400"]
-        for index,i in enumerate(self.lines):
+        for index, i in enumerate(self.lines):
             if i == "M03":
                 self.lines = self.lines[index:]
                 break
-            
+             
         for i in self.defult[::-1]:
             self.lines.insert(0,i)
-        self.display()   
-    
+        self.display()  
+   
     def add(self):
         pass
-        
-    def change(self):
-        pass
-    
+       
+    def change(self, change):
+        self.change_index = []
+        self.change_index.clear()
+
+
+        if change[0:3] == "G00 Z":
+            self.prefix = "G00 Z"
+        elif change[0:3] == "G01 Z":
+            self.prefix = "G01 Z"    
+       
+        for index, i in enumerate(self.lines):
+            if i == self.prefix:
+                self.change_index.append(index)
+        for i in self.change_index:
+            self.lines[i][4:] = change[4:]
+   
+    def has_feed(self, inp):
+        self.checked_lines = []
+        self.lines_with_feed = []
+        if inp[0:3] == "G01 X":
+            self.search_for = "G01 X"
+            
+        for index, i in enumerate(self.lines):
+            if i[0:3] == self.search_for:
+                words = i.split()    
+                if len(words) >= 2:
+                        self.lines[index] = " ". join(words[:2])
+               
+        self.display()
+
+
     def run(self):
         self.root.mainloop()
        
 if __name__ == "__main__":
     convert = trace_editor()
     convert.run()
+
+
+
+
 
